8dc42af3b997242289e6ca34ce5698a2f540a489
Stas <diesel_e-mail.ru@mail.ru>
2023-03-18 00:29:09 +0300

    Lesson 3
    Lessin 3

---
+++ src/Main.java
@@ -0,0 +1,173 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //1
+//        int a = 10;
+//
+//        if (a == 10){
+//            System.out.println("yes");
+//           }else System.out.println("no");
+
+        //2
+
+
+
+//int min = 30;
+//
+//    if (min>=0 && min <= 15) {
+//        System.out.println("1 chetvert");
+//    }else if(min>=15 && min <= 30){
+//        System.out.println("2 chetvert");
+//    }else if(min>=30 && min <= 45){
+//        System.out.println("3 chetvert");
+//    }else{
+//        System.out.println("4 chetvert");
+//    }
+//
+//    System.out.println(min);
+
+        //3
+//int a = 0;
+//        if (a < 0) {
+//            System.out.println("verno");
+//}else System.out.println("neverno");
+
+
+
+//        int a = 1;
+//        if (a < 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+//        int a = -3;
+//        if (a < 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+        //4
+//int a = 0;
+//        if (a > 0) {
+//            System.out.println("verno");
+//}else System.out.println("neverno");
+
+
+
+//        int a = 1;
+//        if (a > 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+//        int a = -3;
+//        if (a > 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+
+        //5
+//int a = 0;
+//        if (a >= 0) {
+//            System.out.println("verno");
+//}else System.out.println("neverno");
+
+
+
+//        int a = 1;
+//        if (a >= 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+//        int a = -3;
+//        if (a >= 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+        //6
+//int a = 0;
+//        if (a <= 0) {
+//            System.out.println("verno");
+//}else System.out.println("neverno");
+
+
+
+//        int a = 1;
+//        if (a <= 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+//        int a = -3;
+//        if (a <= 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+        //7
+//int a = 0;
+//        if (a != 0) {
+//            System.out.println("verno");
+//}else System.out.println("neverno");
+
+
+
+//        int a = 1;
+//        if (a != 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+
+//        int a = -3;
+//        if (a != 0) {
+//            System.out.println("verno");
+//        }else System.out.println("neverno");
+
+        //10
+//        int a = 10;
+//        int b = 20;
+//        int c = 30;
+//
+//        if (a>b && a>c){
+//            System.out.println("a max");
+//        }
+//
+//        else if(b>a && b>c){
+//            System.out.println("b max");
+//        }
+//        else if(c>a && c>b)System.out.println("c max ");
+//
+//        if (a<b && a<c ){
+//            System.out.println("a min");
+//        }
+//
+//        else if(b<a && b<c){
+//            System.out.println("b min");
+//        }
+//        else if(c<a && c<b)System.out.println("c min ");
+
+
+        //11
+//        int a = -5;
+//
+//        if(a > 0){
+//            System.out.println("polojitelnoe");
+//        } else System.out.println("otrizatelnoe");
+
+
+        //12
+
+//        int a = 11;
+//
+//        if(a % 2 == 0) {
+//            System.out.println("chetnoe");
+//        } else System.out.println("ne chetnoe");
+
+
+
+
+
+    }
+}
